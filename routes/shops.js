1b67f97b20a9e223c6adba4297815968b74b866c
hu-qi <huqi1024@gmail.com>
2024-03-14 02:33:15 +0800

    core: modify api.

--- routes/shops.js
+++ routes/shops.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const shop_controller = require('../controllers/shopController')
 
-/* GET users listing. */
+/* GET Shops listing. */
 router.get('/', shop_controller.get_shop_list);
 
 module.exports = router;
